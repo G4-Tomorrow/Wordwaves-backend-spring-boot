d3d8aa96424763dddc0900bd6bb99d3e8c2400bf
Tran Hoang Quan <quan02042004@gmail.com>
2024-10-05 14:49:05 +0700

    #13 - :bug:refactor: add permission for ./mvnw

--- src/main/java/com/server/wordwaves/config/CustomJwtDecoder.java
+++ src/main/java/com/server/wordwaves/config/CustomJwtDecoder.java
@@ -38,6 +38,9 @@ public class CustomJwtDecoder implements JwtDecoder {
 
 
 
+
+
+
         try {
             SignedJWT signedJWT = SignedJWT.parse(token);
             JWTClaimsSet jwtClaimsSet = signedJWT.getJWTClaimsSet();
