7f20b2e42824f0bc5f6e38d25d09f0e48cdb7e33
Tran Hoang Quan <quan02042004@gmail.com>
2024-10-12 13:54:55 +0700

    :chart_with_upwards_trend:feat: map data into wordResponse when get from topic

--- src/main/java/com/server/wordwaves/service/implement/TopicServiceImp.java
+++ src/main/java/com/server/wordwaves/service/implement/TopicServiceImp.java
@@ -1,14 +1,5 @@
 package com.server.wordwaves.service.implement;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
-
 import com.server.wordwaves.dto.request.vocabulary.TopicCreationRequest;
 import com.server.wordwaves.dto.response.common.Pagination;
 import com.server.wordwaves.dto.response.common.PaginationInfo;
@@ -24,13 +15,21 @@ import com.server.wordwaves.mapper.TopicMapper;
 import com.server.wordwaves.mapper.WordMapper;
 import com.server.wordwaves.repository.TopicRepository;
 import com.server.wordwaves.repository.WordCollectionRepository;
+import com.server.wordwaves.repository.httpclient.DictionaryClient;
 import com.server.wordwaves.service.TopicService;
 import com.server.wordwaves.utils.MyStringUtils;
-
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -40,6 +39,7 @@ public class TopicServiceImp implements TopicService {
     TopicRepository topicRepository;
     WordCollectionRepository wordCollectionRepository;
     TopicMapper topicMapper;
+    DictionaryClient dictionaryClient;
     WordMapper wordMapper;
 
     @Override
@@ -66,8 +66,8 @@ public class TopicServiceImp implements TopicService {
         Sort sort = MyStringUtils.isNullOrEmpty(sortBy)
                 ? Sort.unsorted()
                 : sortDirection.equalsIgnoreCase("DESC")
-                        ? Sort.by(sortBy).descending()
-                        : Sort.by(sortBy).ascending();
+                ? Sort.by(sortBy).descending()
+                : Sort.by(sortBy).ascending();
         Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
 
         Page<Word> wordPage;
@@ -78,8 +78,18 @@ public class TopicServiceImp implements TopicService {
             wordPage = topicRepository.findWordsByTopicId(topicId, pageable);
         }
 
-        List<WordResponse> wordResponses =
-                wordPage.map(wordMapper::toWordResponse).toList();
+        List<WordResponse> wordResponses = wordPage.map(word -> {
+            List<WordResponse> tmp = dictionaryClient.retrieveEntries(word.getName());
+            WordResponse wordResponse = tmp.getFirst();
+            wordResponse.setId(word.getId());
+            wordResponse.setName(word.getName());
+            wordResponse.setThumbnailUrl(word.getThumbnailUrl());
+            wordResponse.setCreatedAt(word.getCreatedAt());
+            wordResponse.setUpdatedAt(word.getUpdatedAt());
+            wordResponse.setCreatedById(word.getCreatedById());
+            wordResponse.setVietnamese(word.getVietnamese());
+            return wordResponse;
+        }).toList();
 
         return PaginationInfo.<List<WordResponse>>builder()
                 .pagination(Pagination.builder()
@@ -93,7 +103,7 @@ public class TopicServiceImp implements TopicService {
                         .sortDirection(sortDirection)
                         .searchQuery(searchQuery)
                         .build())
-                .data(wordPage.map(wordMapper::toWordResponse).toList())
+                .data(wordResponses)
                 .build();
     }
 }
