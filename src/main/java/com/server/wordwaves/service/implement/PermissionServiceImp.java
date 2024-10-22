797729f179dba007b858f0641aeb4340535d1d2b
Quách Phú Thuận <thuanmobile1111@gmail.com>
2024-10-22 10:09:28 +0700

    refactor: Reusable get alls function for module users, permissions and roles & Valite query

--- src/main/java/com/server/wordwaves/service/implement/PermissionServiceImp.java
+++ src/main/java/com/server/wordwaves/service/implement/PermissionServiceImp.java
@@ -1,18 +1,14 @@
 package com.server.wordwaves.service.implement;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
-import com.server.wordwaves.service.BaseService;
-import org.springframework.data.domain.*;
+import com.server.wordwaves.utils.PaginationUtils;
 import org.springframework.stereotype.Service;
 
 import com.server.wordwaves.dto.request.permission.PermissionCreationRequest;
 import com.server.wordwaves.dto.request.permission.PermissionUpdateRequest;
-import com.server.wordwaves.dto.response.common.Pagination;
 import com.server.wordwaves.dto.response.common.PaginationInfo;
-import com.server.wordwaves.dto.response.common.QueryOptions;
 import com.server.wordwaves.dto.response.permission.PermissionResponse;
 import com.server.wordwaves.entity.user.Permission;
 import com.server.wordwaves.exception.AppException;
@@ -33,13 +29,10 @@ public class PermissionServiceImp implements PermissionService {
 
     PermissionRepository permissionRepository;
     PermissionMapper permissionMapper;
-    BaseService<Permission, PermissionResponse> baseService;
 
     @Override
     public PermissionResponse createPermission(PermissionCreationRequest request) {
-        if (permissionRepository.existsByName(request.getName())) {
-            throw new AppException(ErrorCode.PERMISSION_EXISTED);
-        }
+        if (permissionRepository.existsByName(request.getName())) throw new AppException(ErrorCode.PERMISSION_EXISTED);
         Permission permission = permissionMapper.toPermission(request);
         Permission savedPermission = permissionRepository.save(permission);
         return permissionMapper.toPermissionResponse(savedPermission);
@@ -47,15 +40,13 @@ public class PermissionServiceImp implements PermissionService {
 
     @Override
     public PermissionResponse getPermissionByName(String name) {
-        Permission permission = permissionRepository.findById(name)
-                .orElseThrow(() -> new AppException(ErrorCode.PERMISSION_NOT_EXISTED));
+        Permission permission = permissionRepository.findById(name).orElseThrow(() -> new AppException(ErrorCode.PERMISSION_NOT_EXISTED));
         return permissionMapper.toPermissionResponse(permission);
     }
 
     @Override
     public PermissionResponse updatePermission(String name, PermissionUpdateRequest request) {
-        Permission permission = permissionRepository.findById(name)
-                .orElseThrow(() -> new AppException(ErrorCode.PERMISSION_NOT_EXISTED));
+        Permission permission = permissionRepository.findById(name).orElseThrow(() -> new AppException(ErrorCode.PERMISSION_NOT_EXISTED));
         permission.setDescription(request.getDescription());
         Permission updatedPermission = permissionRepository.save(permission);
         return permissionMapper.toPermissionResponse(updatedPermission);
@@ -63,30 +54,21 @@ public class PermissionServiceImp implements PermissionService {
 
     @Override
     public void deletePermission(String name) {
-        if (!permissionRepository.existsById(name)) {
-            throw new AppException(ErrorCode.PERMISSION_NOT_EXISTED);
-        }
+        if (!permissionRepository.existsById(name)) throw new AppException(ErrorCode.PERMISSION_NOT_EXISTED);
         permissionRepository.deleteById(name);
     }
 
     @Override
     public PaginationInfo<List<PermissionResponse>> getPermissions(int pageNumber, int pageSize, String sortBy, String sortDirection, String searchQuery) {
+        List<String> validSortByFields = Arrays.asList("name", "description", "createdAt");
 
-        Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
-        Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
-
-        Function<String, Page<Permission>> searchFunction = query -> {
-            if (MyStringUtils.isNotNullAndNotEmpty(query)) {
-                return permissionRepository.findByNameContainingOrDescriptionContaining(query, query, pageable);
-            } else {
-                return permissionRepository.findAll(pageable);
-            }
-        };
-
-        Function<Page<Permission>, List<PermissionResponse>> mapFunction = page -> page.stream()
-                .map(permissionMapper::toPermissionResponse)
-                .collect(Collectors.toList());
-
-        return baseService.getAll(pageNumber, pageSize, sortBy, sortDirection, searchQuery, searchFunction, mapFunction);
+        return PaginationUtils.getAllEntities(pageNumber, pageSize, sortBy, sortDirection, searchQuery, validSortByFields,
+                pageable -> {
+                    if (MyStringUtils.isNotNullAndNotEmpty(searchQuery)) return permissionRepository.findByNameContainingOrDescriptionContaining(searchQuery, searchQuery, pageable);
+                    else return permissionRepository.findAll(pageable);
+                },
+                permissionMapper::toPermissionResponse
+        );
     }
+
 }
--- src/main/java/com/server/wordwaves/service/implement/RoleServiceImp.java
+++ src/main/java/com/server/wordwaves/service/implement/RoleServiceImp.java
@@ -1,13 +1,12 @@
 package com.server.wordwaves.service.implement;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import com.server.wordwaves.service.BaseService;
-import org.springframework.data.domain.*;
+import com.server.wordwaves.utils.PaginationUtils;
 import org.springframework.stereotype.Service;
 
 import com.server.wordwaves.dto.request.role.RoleCreationRequest;
@@ -36,13 +35,10 @@ public class RoleServiceImp implements RoleService {
     RoleRepository roleRepository;
     PermissionRepository permissionRepository;
     RoleMapper roleMapper;
-    BaseService<Role, RoleResponse> baseService;
 
     @Override
     public RoleResponse createRole(RoleCreationRequest request) {
-        if (roleRepository.existsByName(request.getName())) {
-            throw new AppException(ErrorCode.ROLE_EXISTED);
-        }
+        if (roleRepository.existsByName(request.getName())) throw new AppException(ErrorCode.ROLE_EXISTED);
         Role role = roleMapper.toRole(request);
         Role savedRole = roleRepository.save(role);
         return roleMapper.toRoleResponse(savedRole);
@@ -50,19 +46,15 @@ public class RoleServiceImp implements RoleService {
 
     @Override
     public RoleResponse getRoleByName(String name) {
-        Role role = roleRepository.findById(name)
-                .orElseThrow(() -> new AppException(ErrorCode.ROLE_NOT_EXISTED));
+        Role role = roleRepository.findById(name).orElseThrow(() -> new AppException(ErrorCode.ROLE_NOT_EXISTED));
         return roleMapper.toRoleResponse(role);
     }
 
     @Override
     public RoleResponse updateRole(String name, RoleUpdateRequest request) {
-        Role role = roleRepository.findById(name)
-                .orElseThrow(() -> new AppException(ErrorCode.ROLE_NOT_EXISTED));
+        Role role = roleRepository.findById(name).orElseThrow(() -> new AppException(ErrorCode.ROLE_NOT_EXISTED));
 
-        if (request.getDescription() != null && !request.getDescription().isEmpty()) {
-            role.setDescription(request.getDescription());
-        }
+        if (request.getDescription() != null && !request.getDescription().isEmpty()) role.setDescription(request.getDescription());
 
         List<String> permissionNames = request.getPermissionNames();
 
@@ -70,33 +62,16 @@ public class RoleServiceImp implements RoleService {
 
         if (permissionNames != null && !permissionNames.isEmpty()) {
             List<Permission> existingPermissions = permissionRepository.findAllById(permissionNames);
-            Set<String> existingPermissionNames = existingPermissions.stream()
-                    .map(Permission::getName)
-                    .collect(Collectors.toSet());
+            Set<String> existingPermissionNames = existingPermissions.stream().map(Permission::getName).collect(Collectors.toSet());
 
-            notExistingPermissions = permissionNames.stream()
-                    .filter(permissionName -> !existingPermissionNames.contains(permissionName))
-                    .collect(Collectors.toList());
+            notExistingPermissions = permissionNames.stream().filter(permissionName -> !existingPermissionNames.contains(permissionName)).collect(Collectors.toList());
 
-            if (!notExistingPermissions.isEmpty()) {
-                throw new AppException(
-                        ErrorCode.PERMISSION_NOT_EXISTED,
-                        "Các quyền hạn sau không tồn tại trên hệ thống: " + String.join(", ", notExistingPermissions)
-                );
-            }
+            if (!notExistingPermissions.isEmpty()) throw new AppException(ErrorCode.PERMISSION_NOT_EXISTED, "Các quyền hạn sau không tồn tại trên hệ thống: " + String.join(", ", notExistingPermissions));
 
-            Set<String> existingPermissionsInRole = role.getPermissions().stream()
-                    .map(Permission::getName)
-                    .collect(Collectors.toSet());
+            Set<String> existingPermissionsInRole = role.getPermissions().stream().map(Permission::getName).collect(Collectors.toSet());
+            List<String> alreadyExistPermissions = permissionNames.stream().filter(existingPermissionsInRole::contains).collect(Collectors.toList());
 
-            List<String> alreadyExistPermissions = permissionNames.stream()
-                    .filter(existingPermissionsInRole::contains)
-                    .collect(Collectors.toList());
-
-            if (!alreadyExistPermissions.isEmpty()) {
-                throw new AppException(ErrorCode.PERMISSION_ALREADY_EXISTED,
-                        "Quyền hạn " + String.join(", ", alreadyExistPermissions) + " đã tồn tại trong vai trò " + name);
-            }
+            if (!alreadyExistPermissions.isEmpty()) throw new AppException(ErrorCode.PERMISSION_ALREADY_EXISTED, "Quyền hạn " + String.join(", ", alreadyExistPermissions) + " đã tồn tại trong vai trò " + name);
 
             role.getPermissions().addAll(existingPermissions);
         }
@@ -108,30 +83,20 @@ public class RoleServiceImp implements RoleService {
 
     @Override
     public void deleteRole(String name) {
-        if (!roleRepository.existsById(name)) {
-            throw new AppException(ErrorCode.ROLE_NOT_EXISTED);
-        }
+        if (!roleRepository.existsById(name)) throw new AppException(ErrorCode.ROLE_NOT_EXISTED);
         roleRepository.deleteById(name);
     }
 
     @Override
     public PaginationInfo<List<RoleResponse>> getRoles(int pageNumber, int pageSize, String sortBy, String sortDirection, String searchQuery) {
+        List<String> validSortByFields = Arrays.asList("name", "description", "createdAt");
 
-        Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
-        Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
-
-        Function<String, Page<Role>> searchFunction = query -> {
-            if (MyStringUtils.isNotNullAndNotEmpty(query)) {
-                return roleRepository.findByNameContainingOrDescriptionContaining(query, query, pageable);
-            } else {
-                return roleRepository.findAll(pageable);
-            }
-        };
-
-        Function<Page<Role>, List<RoleResponse>> mapFunction = page -> page.stream()
-                .map(roleMapper::toRoleResponse)
-                .collect(Collectors.toList());
-
-        return baseService.getAll(pageNumber, pageSize, sortBy, sortDirection, searchQuery, searchFunction, mapFunction);
+        return PaginationUtils.getAllEntities(pageNumber, pageSize, sortBy, sortDirection, searchQuery, validSortByFields,
+                pageable -> {
+                    if (MyStringUtils.isNotNullAndNotEmpty(searchQuery)) return roleRepository.findByNameContainingOrDescriptionContaining(searchQuery, searchQuery, pageable);
+                    else return roleRepository.findAll(pageable);
+                },
+                roleMapper::toRoleResponse
+        );
     }
 }
